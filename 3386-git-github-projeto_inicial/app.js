a8d8145680f147b0c69b2d88e4a1f33e4c88cffa
Raquel Souza <raquelssouza82@gmail.com>
2025-01-21 17:16:39 -0300

    Revert "alterando limite para 100"
    This reverts commit 6700b69530b9388a6316b3e336fc104d2c26fcec.

--- 3386-git-github-projeto_inicial/app.js
+++ 3386-git-github-projeto_inicial/app.js
@@ -1,5 +1,9 @@
 let listaDeNumerosSorteados = [];
+<<<<<<< HEAD
 let numeroLimite = 50;
+=======
+let numeroLimite = 10;
+>>>>>>> parent of 6700b69 (alterando limite para 100)
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -11,7 +15,11 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
+<<<<<<< HEAD
     exibirTextoNaTela('p', 'Escolha um número entre 1 e 50');
+=======
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+>>>>>>> parent of 6700b69 (alterando limite para 100)
 }
 
 exibirMensagemInicial();
